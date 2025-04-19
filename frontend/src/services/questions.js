dee03c4d085f70ce8b1a9a6d96b78dd2ce7d0b37
Aditya giri <145798891+GiriAditya14@users.noreply.github.com>
2025-04-20 02:21:14 +0530

    Update questions.js

--- frontend/src/services/questions.js
+++ frontend/src/services/questions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/question';
+const API_URL = '${import.meta.env.VITE_API_URL}/api/question';
 
 const createQuestion = async (text, imageUrl, token) => {
   const config = {
